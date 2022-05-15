6652edda27c46b4689f52ff22d4a4768ddaa6e02
Anup81 <anupdhakal81@gmail.com>
2022-05-15 17:10:10 +0545

    Jump Animation and Animation States Added

--- script.js
+++ script.js
@@ -11,19 +11,51 @@ playerImage.src = "shadow_dog.png";
 
 const spriteWidth = 575; //Width Divide by Number
 const spriteHeight = 523; //Height Divide by Number
-let frameX = 0;
-let frameY = 0;
+// let frameX = 0;
+// let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5; // Variable Declared for Slowing the animation used in the line 35.
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+
+// console.log(animationStates);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // To clear whole canvas
   //   ctx.fillRect(100, 50, 100, 100);
   //   ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dy); // S - source, D - Destination
+
+  let position =
+    Math.floor(gameFrame / staggerFrames) % spriteAnimations["jump"].loc.length; //We'll have to increase the game frame 5 times i.e 1/5 = 0.2, 2/5 = 0.4, 3/5 = 0.6...5/5 = 1
+  // And 1%6 = 1
+
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations["jump"].loc[position].y;
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
-    frameY * spriteHeight,
+    frameX,
+    frameY,
     spriteWidth,
     spriteHeight,
     0,
